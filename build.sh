59e4cd4187f014278cd39d7efd2e95e1d0f4efa4
www-data <www-data@gn178.zone.eu>
2014-09-17 14:30:37 +0300

    Change the builder to make flat zip file.

--- build.sh
+++ build.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-rm -f cybercat-another_autumn.zip
-mkdir cybercat-another_autumn
 
-cp -r bower_components images lib models music src index.html cybercat-another_autumn
+zip cybercat-another_autumn.zip -9 -r bower_components images lib models music src index.html FILE_ID.DIZ
 
-zip cybercat-another_autumn.zip -9 -r cybercat-another_autumn
-
-rm -rf cybercat-another_autumn/
